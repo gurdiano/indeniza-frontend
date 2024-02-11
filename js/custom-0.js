d1e1c8a3f901b90a8c1be74a35dc3758c27330fa
gurdiano <tiagofran2013@hotmail.com>
2024-02-10 23:59:31 -0300

    services-details-page

--- js/custom-0.js
+++ js/custom-0.js
@@ -1,5 +1,7 @@
 const drop_area = document.querySelector('.drag-area');
 const drag_text = drop_area.querySelector("header");
+const file_input = document.getElementById('file-input');
+const file_list = document.getElementById('file-list'); 
 
 drop_area.addEventListener(
   'dragover', event=>{
@@ -7,7 +9,7 @@ drop_area.addEventListener(
     drop_area.classList.add('active');
     drag_text.textContent = 'Release to upload file';
   }
-)
+);
 
 drop_area.addEventListener(
   'dragleave', event=>{
@@ -15,4 +17,43 @@ drop_area.addEventListener(
     drop_area.classList.remove('active');
     drag_text.textContent = 'Drag and drop files here';
   }
-)
+);
+
+drop_area.addEventListener(
+  'drop', event=>{
+    event.preventDefault();
+    drop_area.classList.remove('active');
+    drag_text.textContent = 'Drag and drop files here';
+
+    const files = event.dataTransfer.files;
+    handleFiles(files);
+  }
+);
+
+file_input.addEventListener('change', () => {
+  const files = file_input.files;
+  handleFiles(files);
+});
+
+function handleFiles(files) {
+  file_list.innerHTML = '';
+
+  for (let i = 0; i < files.length; i++) {
+    const fileName = files[i].name;
+
+    const div_mother = document.createElement('div');
+    div_mother.classList.add('el-0');
+
+    const div_child_0 = document.createElement('div');
+    div_child_0.classList.add('custom-icon-3');
+
+    const div_child_1 = document.createElement('div');
+    div_child_1.classList.add('el-1');
+
+    div_mother.appendChild(div_child_0);
+    div_mother.appendChild(div_child_1);
+    file_list.appendChild(div_mother);
+
+    div_child_1.textContent = fileName;
+  }
+}
